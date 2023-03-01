49a466a0c5d5ff6c32d54868b7b46e497df9b9cf
IT21167614 <92454064+Nimsaramahagedara@users.noreply.github.com>
2023-03-01 12:35:26 +0530

    viewportFix

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ const menuItem = document.getElementById("menuItems");
 menuBtn.addEventListener("click", menuToggle);
 console.log(menuItem);
 function menuToggle (){
-    if(menuItem.style.height == "0px"){
+    if(menuItem.style.height == "0px" || menuItem.style.height == ""){
         menuItem.style.height = "190px";
         
     }else{
