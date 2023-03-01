bdc8891f9dd0bbda96c30c34a5796c31a23c947a
IT21167614 <92454064+Nimsaramahagedara@users.noreply.github.com>
2023-03-01 12:04:35 +0530

    Ecommerce Site-Static

---
+++ script/script.js
@@ -0,0 +1,14 @@
+const menuBtn = document.getElementById("menuBtn");
+const menuItem = document.getElementById("menuItems");
+menuBtn.addEventListener("click", menuToggle);
+console.log(menuItem);
+function menuToggle (){
+    if(menuItem.style.height == "0px"){
+        menuItem.style.height = "190px";
+        
+    }else{
+        menuItem.style.height = "0px";
+    }
+}
+
+  
